2bcf4ee73ac1a38b5440ba26532d3ebbe93f2129
kevin1010607 <kevin.1010607@gmail.com>
2023-01-18 16:34:02 +0800

    update HW3 experiment

--- HW3/experiment/Weak Scalability/job.sh
+++ HW3/experiment/Weak Scalability/job.sh
@@ -4,8 +4,11 @@
 #SBATCH -n 2
 #SBATCH -G 2
 
-IN1=/home/pp22/share/hw3-2/cases/p25k1
-IN2=/home/pp22/share/hw3-3/cases/p35k1
+# Time complexity: O(n^3)
+# 1 GPU, n1 = 34000, n1*n1*n1 = 3.930e13
+IN1=/home/pp22/share/hw3-2/cases/p34k1
+# 2 GPU, n2 = 42793, n2*n2*n2 = 7.836e13 ≈ 2*n1*n1*n1
+IN2=/home/pp22/share/hw3-2/cases/p43k1
 OUT=/dev/null
 
 # Compile code
